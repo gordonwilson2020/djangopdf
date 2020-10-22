e751bcb5c10554c5ebc971c713ae605091deae8d
gordonwilson2020 <gwilsonia2020@gmail.com>
2020-10-22 14:51:50 +0100

    initial commit

---
+++ djangoPdf/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+
+from django.views.generic import View
+
+from django.template.loader import get_template
+
+from .utils import render_to_pdf
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('invoice.html')
+        context = {
+            "invoice_id": 123,
+            "customer_name": "John Cooper",
+            "amount": 1399.99,
+            "today": "Today",
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('invoice.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" %("123412341")
+            content = "inline; filename ='%s'" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" %(filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
